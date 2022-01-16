21c84d3eb68b6a0bbe86d7bdd6acc691ce955d61
Сергей <gan7er@yandex.ru>
2022-01-16 12:50:41 +0300

    add property in model

--- SafeDevelopHomeWork-1/SafeDevelopHomeWork-1/Models/CardModel.cs
+++ SafeDevelopHomeWork-1/SafeDevelopHomeWork-1/Models/CardModel.cs
@@ -2,5 +2,12 @@
 {
     public class CardModel
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Famaly { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime ValidPeriod { get; set; }
+        public int NomberCard { get; set; }
+        public int CCV { get; set; }
     }
 }
