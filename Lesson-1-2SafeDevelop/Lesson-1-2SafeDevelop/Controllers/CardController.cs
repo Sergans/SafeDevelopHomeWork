c40a380cf474939df6b41ac6954f273796b883ae
Сергей <gan7er@yandex.ru>
2022-01-18 19:07:11 +0300

    refact

---
+++ Lesson-1-2SafeDevelop/Lesson-1-2SafeDevelop/Controllers/CardController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Lesson_1_2SafeDevelop.Controllers
+{
+    [Route("api/card")]
+    [ApiController]
+    public class CardController : ControllerBase
+    {
+        // GET: api/<CardController>
+        [HttpGet("get")]
+        public IActionResult Get()
+        {
+            return Ok("Готово");
+        }
+
+        // GET api/<CardController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<CardController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<CardController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<CardController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
